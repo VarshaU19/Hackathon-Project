932cc14c3cc9bfcb737f54ffeb831966fef626a7
VarshaU19 <var.1suth@gmail.com>
2025-11-16 01:28:06 -0500

    modified:   hackathon-project/src/App.css 	modified:   hackathon-project/src/DrawingCanvas.jsx

--- hackathon-project/src/DrawingCanvas.jsx
+++ hackathon-project/src/DrawingCanvas.jsx
@@ -2,19 +2,18 @@ import CanvasPaint from 'react-canvas-paint';
 import 'react-canvas-paint/dist/index.css';
 import './App.css';
 
-
 export function PaintCanvas(){
 
     return (
         <div id="CanvasBox">
             <h1 id="canvasTitle">Drawing Canvas</h1>
-            <CanvasPaint className="CanvasArea"
-                width={600}
-                height={650}
+            <CanvasPaint id="CanvasArea"
+                width={screen.width * 0.5}
+                height={screen.height * 0.5}
                 brushColor={"#000000"}
                 brushRadius={5}
-                lazyRadius={12}
-                colors={["#000000", "#ff0000", "#00ff00", "#0000ff"]}
+                lazyRadius={0}
+                colors={["#000000", "#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff", "#ffffff", "#008000", "#800080", "#570303ff", "#000080"]}
             />
         </div> 
     )    
